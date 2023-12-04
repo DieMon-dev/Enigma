15a558e5200cc2f23afa7b34e83a9543c5812812
Dmytro Nakutnii <72558879+DieMon-dev@users.noreply.github.com>
2023-12-04 21:41:18 +0100

    One test controller for login2

---
+++ api/src/main/java/com/DieMoon/EnigmaChat/EnigmaChatApplication.java
@@ -0,0 +1,13 @@
+package com.DieMoon.EnigmaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaChatApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/DieMoon/EnigmaChat/EnigmaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DieMoon.EnigmaChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigmaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
