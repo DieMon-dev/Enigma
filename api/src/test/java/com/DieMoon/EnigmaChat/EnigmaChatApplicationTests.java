cc2ba9891915a8959fdcc4b14898cf0c1c663884
Dmytro Nakutnii <72558879+DieMon-dev@users.noreply.github.com>
2023-12-06 18:21:24 +0100

    Added User sevices and controllers:  + to check if  user login exists  + to get user by id  + to get user by login  + to create new user  + to delete user

--- api/src/test/java/com/DieMoon/EnigmaChat/EnigmaChatApplicationTests.java
+++ api/src/test/java/com/DieMoon/EnigmaChat/EnigmaChatApplicationTests.java
@@ -1,13 +1,72 @@
 package com.DieMoon.EnigmaChat;
 
+import com.DieMoon.EnigmaChat.models.User;
+import com.DieMoon.EnigmaChat.services.UserService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.ExpectedCount.times;
+import com.DieMoon.EnigmaChat.models.User;
+import com.DieMoon.EnigmaChat.services.UserService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 @SpringBootTest
 class EnigmaChatApplicationTests {
+	@Mock
+	private FirebaseAuth firebaseAuth;
 
+	@InjectMocks
+	private UserService userService;
+
+	public void UserServiceTest() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+//	@Test
+//	public void testCreateUser() throws Exception {
+//		// Arrange
+//		User user = new User();
+//		user.setUserPhoneNumber("test@example.com");
+//		user.setUserPassword("testPassword");
+//		user.setUserName("Test User");
+//
+//		UserRecord.UpdateRequest updateRequest = new UserRecord.UpdateRequest()
+//				.setUid("testUserId")
+//				.setEmail(user.getUserPhoneNumber())
+//				.setDisplayName(user.getUserName());
+//
+//		when(firebaseAuth.createUser(any())).thenReturn(updateRequest);
+//
+//		// Act
+//		User createdUser = userService.registerUser(user);
+//
+//		// Assert
+//		verify(firebaseAuth, times(1)).createUser(any());
+//
+//		// Add more assertions based on your expected behavior
+//		// For example, you might want to check if the createdUser has the correct UID
+//
+//		// Example:
+//		// assertEquals("testUserId", createdUser.getUserId());
+//	}
 	@Test
 	void contextLoads() {
+
 	}
 
 }
