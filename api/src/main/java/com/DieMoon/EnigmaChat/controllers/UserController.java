556cdf484860982d2a81cf93963d31b00ca2a853
Dmytro Nakutnii <72558879+DieMon-dev@users.noreply.github.com>
2024-01-16 21:54:39 +0100

    Removed useless if in getUserByLogin

--- api/src/main/java/com/DieMoon/EnigmaChat/controllers/UserController.java
+++ api/src/main/java/com/DieMoon/EnigmaChat/controllers/UserController.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import static java.lang.Character.isDigit;
 
 @RestController
 @RequestMapping("/api/users")
