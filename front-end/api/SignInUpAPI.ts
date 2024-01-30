a8bc919a8e81a370b28d4e15cc7bd8fa42745843
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2024-01-30 17:47:52 +0100

    auto rerendering of chat was added. (settimeout())

--- front-end/api/SignInUpAPI.ts
+++ front-end/api/SignInUpAPI.ts
@@ -4,7 +4,7 @@ import { runInAction } from 'mobx';
 
 export default class  EnigmaAPI{
 
-    private url = "https://ten-planets-share.loca.lt"
+    private url = "https://cool-snails-lick.loca.lt"
 
     private url_login = this.url + "/api/users/check/userPassword/"
     private url_login_register = this.url + "/api/users/check/userLogin/"
